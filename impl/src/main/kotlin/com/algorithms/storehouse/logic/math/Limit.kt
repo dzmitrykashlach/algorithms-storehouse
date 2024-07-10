7e7d2d01458a149b75f27635ecbc65efa6a6206f
dzmitrykashlach <dzmitrykashlach@pm.me>
2024-07-10 14:33:26 +0300

    init

---
+++ impl/src/main/kotlin/com/algorithms/storehouse/logic/math/Limit.kt
@@ -0,0 +1,15 @@
+package com.algorithms.storehouse.logic.math
+
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+
+class Limit(val f: (Double) -> Double) {
+
+    fun calculate(from: Double, to: Double, accuracy: Double): Double {
+        var x = from
+            while (abs(f(x) - f((x + to) / 2)) >= accuracy) {
+                x = (x + to) / 2
+            }
+        return f(x)
+    }
+}
